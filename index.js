5f65b3bfa40418706fdfd694cf2ca02e308d5b0b
Niang Khawm Huai <khawmhuai@gmail.com>
2022-02-24 12:53:52 +0000

    update

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-//Getting all input text objects
-
 var username = document.forms["vform"]["username"];
 var email = document.forms["vform"]["email"];
 var age = document.forms["vform"]["age"];
 var password = document.forms["vform"]["password"];
 var password_confirmation = document.forms["vform"]["password_confirmation"];
+var eValidation =  /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 
 //Error display objects
 
@@ -14,8 +13,11 @@ var age_error = document.getElementById("age_error");
 var password_error = document.getElementById("password_error");
 
 //Event listeners
+/*submit.addEventListener ('click',(e) =>{
+    e.preventDefault();
+});*/
 
-username.addEventListener("blur", nameVerify,true);
+username.addEventListener("blur", usernameVerify,true);
 
 email.addEventListener("blur", emailVerify,true);
 
@@ -28,56 +30,78 @@ password.addEventListener("blur", passwordVerify,true);
 function Validate(){
     //username
     if(username.value ===''){
-        username.style.border = "2px solid red";
-        document.getElementById('username_div').style.colr='red';
+        username.style.border = "3px solid red";
+        document.getElementById('username_div').style.color='black';
         name_error.textContent = "Username is required";
         username.focus();
         return false;
     }
     //email
-    if(email.value ===''){
-        email.style.border = "2px solid red";
-        email_error.textContent = "Email is invalid";
+    if(email.value ==='' ||!eValidation.test(email.value)){
+        email.style.border = "3px solid red";
+        document.getElementById('email_div').style.color='black';
+        email_error.textContent = " Valid email is required";
         email.focus();
         return false;
     }
+     /*if (email.value.method(eValidation)){
+        email.style.border = "3px solid red";
+        document.getElementById('email_div').style.color='red';
+        email.error.textContext = " Email is invalid"
+        email.focus();
+        return false;
+    } */
 
     //age
     if(age.value ===''){
-        age.style.border = "2px solid red";
+        age.style.border = "3px solid red";
+        document.getElementById('age_div').style.color='black';
         age_error.textContent = "Age can not be blank";
         age.focus();
         return false;
     }
 
     //password
-    if(password.value ===''){
-        password.style.border = "2px solid red";
-        password_error.textContent = "password doesn't match";
+    if(password.value ==='' ||password.value != password_confirmation.value ){
+        password.style.border = "3px solid red";
+        document.getElementById('password_div').style.color='black';
+        password_error.textContent = "password do no match";
         password.focus();
         return false;
     }
-    if(password.value != password_comfirmation.value){
-        password.style.border ='2px solid red';
+    
+    /*if(password.value != password_confirmation.value){
+        password.style.border ='3px solid red';
+        document.getElementById('password_div').style.color='black';
         password_confirmation.style.border = '2px solid red';
         password_error.textContent='Password does not match';
         return false;
+    }*/
+      if  (password.value.length < 8){
+        password.style.border ='3px solid red';
+        document.getElementById('password_div').style.color='black';
+        password_confirmation.style.border = '2px solid red';
+        password_error.textContent='Password must contain at least 8';
+        password.focus();
+        return false;
     }
+
+    
 }
 
 //Event handler function
 function usernameVerify(){
     if (username !=""){
-        username.style.border = "2px solid #5e6e66"
-        username_error.innerHTML="";
+        username.style.border = "3px solid #5e6e"
+        name_error.innerHTML="";
         return true;
     }
     
 }
 
-function emailVerify(){
-    if (email !=""){
-        email.style.border = "2px solid #5e6e66"
+function emailVerify(value){
+     if (eValidation.test(email.value)){
+        email.style.border = "3px solid #5e6e"
         email_error.innerHTML="";
         return true;
     }
@@ -86,20 +110,45 @@ function emailVerify(){
 
 function ageVerify(){
     if (age !=""){
-        age.style.border = "2px solid #5e6e66"
+        age.style.border = "3px solid #5e6e"
         age_error.innerHTML="";
         return true;
     }
     
+    
 }
 
 function passwordVerify(){
-    if (password !=""){
-        password.style.border = "2px solid #5e6e66"
+    if (password !="" || password.value.lenght >= 8){
+        password.style.border = "3px solid #5e6e"
         password_error.innerHTML="";
         return true;
     }
     
+    
 }
+function passwordVerify() {
+  if (password.value != "") {
+  	password.style.border = "3px solid #5e6e";
+  	document.getElementById('pass_confirm_div').style.color = "#5e6e66";
+  	document.getElementById('password_div').style.color = "#5e6e66";
+  	password_error.innerHTML = "";
+  	return true;
+  }
+  if (password.value === password_confirmation.value) {
+  	password.style.border = "3px solid #5e6e";
+  	document.getElementById('pass_confirm_div').style.color = "#5e6e66";
+  	password_error.innerHTML = "";
+  	return true;
+  }
+  if (password.value.lenght >= 8){
+    password.style.border = "3px solid #5e6e";
+  	document.getElementById('password_div').style.color = "#5e6e66";
+  	password_error.innerHTML = "";
+  	return true;
+
+  }
+}
+
 
 
